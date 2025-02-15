da649421238ba66ff6cf69915d74101fd4aa6ecd
ananthi26 <ananthignanavel8@gmail.com>
2025-02-15 21:38:45 +0530

    Files are added

--- src/task_5/custom_exception_example.java
+++ src/task_5/custom_exception_example.java
@@ -1,5 +1,22 @@
 package task_5;
-
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);  
+    }
+}
 public class custom_exception_example {
-
+    public static void main(String[] args) {
+        try {
+            validateAge(15);  
+        } catch (InvalidAgeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or older, provided: " + age);
+        } else {
+            System.out.println("Age is valid.");
+        }
+    }
 }
--- src/task_5/interface_example.java
+++ src/task_5/interface_example.java
@@ -1,5 +1,29 @@
 package task_5;
-
+interface features
+{
+	public void name();
+}
+interface walk
+{
+	void sound();
+}
+class dog implements features,walk
+{
+	public void name()
+	{
+		System.out.println("Dog name is:anvii");
+	}
+	public void sound()
+	{
+		System.out.println("Bow...");
+	}
+}
 public class interface_example {
+	public static void main (String[] args)
+	{
+		dog d=new dog();
+		d.name();
+		d.sound();
+	}
 
 }
